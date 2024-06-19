d0c062e5e56335b2bf594030e965d60537ffcac3
Balamurugank10 <a20cada56@sjctnc.edu.in>
2024-06-19 11:06:05 +0530

    frontend

---
+++ src/components/HomePage.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import {Box, Typography, } from "@mui/material";
+import MovieItem from './Movies/MovieItem';
+import { Link } from 'react-router-dom';
+import { getAllMovies } from '../api-helpers/api-helpers';
+
+const HomePage = () => {
+
+const [movies,setMovies]=  useState([]);
+useEffect(() => {
+getAllMovies()
+.then((data)=> setMovies(data.movies))
+.catch(err => console.log(err));
+},[]); 
+console.log(movies);
+
+  return <Box width={'100%'} height={'100%'} margin={'auto'} marginTop={2}>
+    <Box margin={'auto'} width={'80%'} height={'40vh'} padding={2}>
+        <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*hokOn8W6gzNEvh9jl-2Kxw.jpeg" alt="kalki" height={'100%'} width={'100%'}
+/>    </Box>
+<Box padding={5}  margin={'auto'} >
+    <Typography variant='h4' textAlign={'center'}>Latest Releases</Typography>
+</Box>
+<Box display="flex" width="80%" justifyContent={'center'} flexWrap="wrap" margin={'auto'} alignItems="center">
+    {movies && movies.map((movie,index) => 
+    (<MovieItem 
+    id={movie.id}
+    title={movie.title}
+    posterUrl={movie.posterUrl}
+    releaseDate={movie.releaseDate}
+    key={index} />))}
+</Box>
+<Box display={'flex'} padding={5} margin='auto'>
+  <Button LinkComponent={Link} to="/movies"  variant="outlined"  sx={{margin:'auto', color:"#2b2d42"}}>
+    View All Movies
+   
+  </Button>
+</Box>
+  </Box>;
+}; 
+
+export default HomePage
